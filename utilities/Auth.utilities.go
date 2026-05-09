aecbe503412959734d2bdbb1e3209cf5907055f1
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-05-08 21:34:00 -0400

    Added SameSite=None to Set-Cookie header

--- utilities/Auth.utilities.go
+++ utilities/Auth.utilities.go
@@ -107,5 +107,5 @@ func GenerateJWT(username string) (string, error) {
 }
 
 func GetSetCookieHeaderValue(tokenString string) string {
-	return "token=" + tokenString + "; HttpOnly; Path=/; Max-Age=3600"
+	return "token=" + tokenString + "; HttpOnly; Path=/; Max-Age=3600; SameSite=None"
 }
