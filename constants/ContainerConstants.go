f24e5967288f6e836d5b0213eb1977ec9fd2c8db
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-05-25 16:15:29 -0400

    Added password reset to container constants

--- constants/ContainerConstants.go
+++ constants/ContainerConstants.go
@@ -3,3 +3,4 @@ package constants
 const USER_CONTAINER_NAME = "users"
 const LINK_CONTAINER_NAME = "links"
 const CLICK_CONTAINER_NAME = "clicks"
+const PASSWORD_RESET_REQUEST_CONTAINER_NAME = "password-reset-requests"
